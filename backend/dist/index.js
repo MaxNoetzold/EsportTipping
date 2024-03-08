7216577b584f5d251996a2c9dcb79e3492fde050
MaxNoetzold <max.noetzold@gmail.com>
2024-03-08 23:42:48 +0100

    remove dist folder from git

--- backend/dist/index.js
+++
@@ -1,9 +0,0 @@
-import "dotenv/config";
-import expressApp from "./server.js";
-import connectToDatabase from "./mongodb/connectToDatabase.js";
-await connectToDatabase();
-const port = process.env.PORT || 3000;
-expressApp.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-});
-//# sourceMappingURL=index.js.map
--- backend/dist/types/LecEvent.js
+++
@@ -1,2 +0,0 @@
-export {};
-//# sourceMappingURL=LecEvent.js.map
